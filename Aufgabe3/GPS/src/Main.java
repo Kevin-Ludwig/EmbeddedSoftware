1bb0a54b83e8777bf38ebd342135c876aa985c82
Kevin Ludwig <ludwig_kevin@outlook.com>
2019-12-19 12:04:59 +0100

    Added file reader + gps_21 file

--- Aufgabe3/GPS/src/Main.java
+++ Aufgabe3/GPS/src/Main.java
@@ -2,8 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Hello World");
+
+		// cmd: java Main gps_21.txt
+
+		Reader reader = new Reader();
+		int[] gpsSignal =  reader.read("res/gps_21.txt");
+		//	int[] gpsSignal = reader.read(args[0]);
+
+		for(int i = 0; i < gpsSignal.length; i++) {
+			System.out.println(gpsSignal[i]);
+		}
+
 	}
 
 }
---
+++ Aufgabe3/GPS/src/Reader.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Reader {
+
+
+	public int[] read(String path) {
+
+		String input = null;
+
+		try {
+			FileReader fr = new FileReader(path);
+			BufferedReader br = new BufferedReader(fr); 
+			input = br.readLine();
+			br.close();
+		} catch (IOException e) {
+			System.out.println("Can't open file, cancel program!");
+			System.exit(0);
+		}
+
+		String[] splitter = input.split(" ");
+		int[] gpsSignal = new int[splitter.length];
+
+		for (int i = 0; i < gpsSignal.length; i++) {
+			gpsSignal[i] = Integer.parseInt(splitter[i]);
+		}
+		return gpsSignal;
+	}
+
+
+
+}
