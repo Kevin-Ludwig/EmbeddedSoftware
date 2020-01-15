519e1d8cf2a584458672d6908af90598fd90f9ea
Kevin Ludwig <ludwig_kevin@outlook.com>
2020-01-15 19:14:19 +0100

    Added decoder function; GPS works completely

--- Aufgabe3/GPS/src/Decoder.java
+++ Aufgabe3/GPS/src/Decoder.java
@@ -1,13 +1,47 @@
+import java.util.Arrays;
 
 public class Decoder {
 
-	private int[] gpsSignal;
-	private int[][] satCodes;
+	public Decoder() {
 
-	
-	public Decoder(int[] gpsSignal, int[][] satCodes) {
-		this.gpsSignal = gpsSignal;
-		this.satCodes = satCodes;
 	}
-	
+
+	public void decode(int[] gps, int[] satCode, int satID) {
+		int peek = -2 ^ ((1023 + 2) / 2) - 1;
+
+		int[] doubleGps = new int[gps.length * 2];		// dobule gps array
+		for(int i = 0; i < gps.length; i++) {
+			doubleGps[i] = gps[i];
+			doubleGps[1023 + i] = gps[i];
+		}
+
+		int maxSum = 0; 
+		int maxOffset = 0;
+
+		for (int offset = 0; offset < gps.length; offset++) {
+			int[] offsetGps = Arrays.copyOfRange(doubleGps, offset, offset + 1023);
+			int sum = product(offsetGps, satCode);
+
+			if (Math.abs(sum) > Math.abs(maxSum)) {
+				maxSum = sum;
+				maxOffset = offset;
+			}
+		}
+
+		if(Math.abs(maxSum) > Math.abs(peek)) {
+			int sendBit = (maxSum > 0) ? 1: 0;
+			System.out.println("Satellite " + (satID + 1) + " has sent bit " + sendBit +
+					" (delta = " + (1023 - maxOffset) + ")");
+		}
+	}
+
+
+	public int product(int [] array1, int[] array2) {
+		int sumOfArrays = 0;
+		for(int i = 0; i < array1.length; i++) {
+			sumOfArrays	+= array1[i] * array2[i];
+		}
+		return sumOfArrays;
+	}
+
 }
--- Aufgabe3/GPS/src/Main.java
+++ Aufgabe3/GPS/src/Main.java
@@ -4,12 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 
 		Reader reader = new Reader();
-		int[] gpsSignal =  reader.read("res/gps_21.txt");	// reader.read(args[0]) ## cmd: java Main gps_21.txt
+		int[] gpsSignal =  reader.read("res/gps_17.txt");	
+		//	int[] gpsSignal =  reader.read(args[0]);	// cmd: java Main gps_21.txt
 
 		GoldCodeGenerator gc = new GoldCodeGenerator();
 		int[][] satelliteCodes = gc.createSatelliteCodes();
 
-		Decoder decode = new Decoder(gpsSignal, satelliteCodes);
-
+		Decoder decoder = new Decoder();
+		for(int satID = 0; satID < satelliteCodes.length; satID++) {
+			decoder.decode(gpsSignal, satelliteCodes[satID], satID);
+		}
 	}
 }
